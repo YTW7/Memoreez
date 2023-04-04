f37d0f465c3b526c486d43ea5728e3cc3f77ba94
YTW7 <myahya96@gmail.com>
2023-04-04 18:15:55 +0530

    initial files

---
+++ constants/index.js
@@ -0,0 +1,3 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const TODO_PROGRAM_PUBKEY = new PublicKey("CqddRvFs18g5v5LanAQmtHmXADaEVbGkuEw5VVUBoHBJ");
---
+++ pages/_app.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import '../styles/global.css'
+
+import dynamic from 'next/dynamic'
+// Import WalletConnectionProvider from components
+// Import the solana wallet css
+// import { WalletConnectionProvider } from '../components/WalletConnectionProvider'
+import "@solana/wallet-adapter-react-ui/styles.css"
+
+
+const WalletConnectionProvider = dynamic(() => import('../components/WalletConnectionProvider'), {
+    ssr: false,
+})
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <Head>
+                <title>Todo App</title>
+            </Head>
+            <main>
+                {/* Wrap provider around App */}
+                <WalletConnectionProvider>
+                <Component {...pageProps} />
+                </WalletConnectionProvider>
+                    
+            </main>
+        </>
+    )
+}
+
+export default MyApp
